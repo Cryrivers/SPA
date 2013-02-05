135eb7783b17a9c8a2d4c55afa117e6443061685
Wang Zhongliang <cryrivers@me.com>
2013-02-05 15:34:48 +0800

    Update RunAutotester and new test files

--- RunAutotester.cmd
+++ RunAutotester.cmd
@@ -1,3 +1,7 @@
 @echo off
-Autotesting\SPA-Autotester.exe Autotesting\SampleCode.c Autotesting\SampleQuery.txt NUL
+@echo Regression Testing (CS3201)
+Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL
+pause
+@echo CS3202 Testing 1
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1Query.txt NUL
 pause
