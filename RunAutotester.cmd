77f3e202861b4535070fe1cf4fcbc9e367b099c9
Wang Zhongliang <cryrivers@me.com>
2013-02-28 05:07:18 +0800

    Add new test cases

--- RunAutotester.cmd
+++ RunAutotester.cmd
@@ -1,5 +1,5 @@
 @echo off
-Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL
+Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL > result.txt
 pause
-Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1Query.txt NUL
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt NUL >> result.txt
 pause
