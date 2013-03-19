2c886097533771cbf9eb839d51757834a944ed72
Yue Cong <yuecongg@gmail.com>
2013-03-19 17:37:12 +0800

    Add analysis.xsl

--- RunAutotester.cmd
+++ RunAutotester.cmd
@@ -1,5 +1,5 @@
 @echo off
 Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL > result.txt
 pause
-Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt result.xml NUL >> result.txt
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt result.xml >> result.txt
 pause
