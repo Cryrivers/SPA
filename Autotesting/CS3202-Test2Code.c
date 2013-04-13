b2004e66c9e2232514fe3a2e30614c9ab861002b
Wang Zhongliang <cryrivers@me.com>
2013-04-13 19:25:17 +0800

    Add TestCode 2

---
+++ Autotesting/CS3202-Test2Code.c
@@ -0,0 +1,315 @@
+procedure Main {
+	com = 10;
+	pure = com + 5;
+	call Ted;
+	ratio = com + pure * 3;
+	never = pure + ratio - com;
+	status = ratio * 12 + never - pure;
+	call See;
+	pit = com * status + never - ratio * pure;
+	while com {
+		com = com -1;
+		ratio = com * status + 3;
+		never = ratio - com +6;
+		if never then {
+			temp = pure + status -pit * 2;
+			dell = temp * never + pit * never - 1;
+			while temp {
+				pure = dell + temp - (ratio * pit + status) + 6 * pure;
+				call Text;
+				idea = pure + dell - temp * pure - com + never;
+				eat = idea - 18;
+				if eat then {
+					front = idea * pit + pure * com - 2 + never;
+					eat = eat - 1;
+				}
+				else {
+					back = idea * status - never * dell - 38 +temp;
+					eat = eat + 1;
+				}
+				call Text;
+				ratio = temp - com;
+				pit = dell + temp * ratio;
+			}
+			temp = temp + 1;
+			dell = dell - 1 * temp + pure - status * 29;
+		}
+		else {
+			temp = pure - pit * 4 - status;
+			dell = never - temp * 9 + (com - pit);
+			call Text;
+			pit = status + 23 - 7 * pure - temp;
+			while dell {
+				pure = 84 + temp * pit - procs * ratio;
+				call See;
+				idea = temp * dell - pure + status -ratio + 48;
+				eat = 15;
+				while eat {
+					left = pit - idea - status * 5 + never - temp;
+					if left then {
+						plain = status - 25 + pit * never;
+						right = pit*never + ratio*pit+status;
+						while right {
+							top = com - pit * temp * dell;
+							if top then {
+								top = top - 75;
+								below = temp * dell + pit * never;
+								call See;
+								right = right - 22;
+							}
+							else {
+								top = top - 35;
+								call Text;
+								below = ratio *pit +status -63;
+								right = right -11;
+							}
+							com =top+pit*never-temp*dell;
+							left = left + 8;
+							idea = com - pit + 25;
+							pure = ratio * pit +status - temp*dell;
+						}
+						call Ted;
+					}
+					else {
+						salt = (com -pit) +idea - 15;
+						slim = pit - never + temp*dell +left;
+						fat = slim * 7;
+						if fat then {
+							call Ted;
+							top = com + (ratio*pit+status);
+							if slim then {
+								red = pit * never;
+								orange = red + pure;
+								yellow = orange - ratio +temp*dell;
+								green = pure+idea-eat;
+								indigo = ratio * pit +status - red;
+								blue = indigo+ green +pit*never;
+								violet= blue+(pure+idea-eat);
+							}
+							else {
+								red=pit - never;
+								orange=red-pure+temp*dell;
+								yellow=red*orange-com;
+								green=status +pit -front;
+								indigo= slim+fat-salt;
+								blue=salt+slim-fat;
+								violet=slim-fat-salt;
+							}
+							warm=red+orange+yellow;
+							cold=green+indigo+blue+violet;
+						}
+						else {
+							fat = fat - 23;
+							slim = slim + 22;
+							if slim then {
+								red = ratio*pit+status;
+								orange = red + salt;
+								yellow = red - ratio +temp*dell;
+								green = pure+idea-eat+salt;
+								indigo = ratio +status - orange;
+								blue = indigo+ green +pit*never;
+								violet= yellow+(pure+idea-eat);
+							}
+							else {
+								red=pit - salt * fat;
+								orange=red-eat+temp*dell + front;
+								yellow=red*orange-back;
+								green=left +yellow -front;
+								indigo= back+plain-salt;
+								blue=salt*plain- idea;
+								violet=eat + back - front * left;
+							}
+							warm=red*orange*yellow;
+							cold=green*indigo*blue*violet;
+						}
+						left = warm *6 + cold * 3;
+						plain = left + eat;
+					}
+					eat = eat - 1;
+				}
+				pure = left + eat;
+				dell = dell - 2;
+				temp = dell - (pure +idea-eat);
+				if temp then {
+					fass=pit * status +(com-pit);
+					science= slim + salt;
+					sde=right + back;
+					engin=plain * front;
+					soc=pure+idea-eat;
+					while fass {
+						temp = never - pure * status;
+						ratio = com + idea;
+						while science {
+							pit = pit - front;
+							while sde {
+								dell = plain - fat * left;
+								while engin {
+									engin = engin -1;
+									while soc {
+										warm = 10+cold; 
+										soc = soc - 1;
+									}
+									call Ted;
+								}
+								sde = sde - 1;
+							}
+							science = science - 1;
+						}
+						temp = ratio + 48;
+						fass = fass - 3;
+					}
+					temp = ratio * pit + status + (com - pit);
+				}
+				else {
+					fass=pit * warm +(com-pit);
+					science= slim + cold +6;
+					sde=right + back - 48 - salt;
+					engin=plain * front - 74 + green;
+					soc=pure+idea-eat+23;
+					if fass then {
+						temp = never - pure * status;
+						ratio = com + idea;
+						if science then {
+							pit = pit - front;
+							if sde then {
+								dell = plain - fat * left;
+								if engin then {
+									engin = engin -1;
+									if soc then {
+										warm = cold*plain; 
+										soc = soc - 1;
+									}
+									else {
+										warm = cold+plain;
+										soc = soc - 2;
+									}
+									call Ted;
+								}
+								else {
+									engin = engin + 1;
+									if soc then {
+										warm = eat*plain; 
+										soc = soc + 1;
+									}
+									else {
+										warm = cold+salt;
+										soc = soc + 2;
+									}
+									call See;
+								}
+								sde = sde - 1;
+							}
+							else {
+								dell = slim * plain +front -back;
+								if soc then {
+									com=pit*never-3;
+									soc = fass+15; }
+								else {
+									ratio = ratio*pit-23+status;
+									soc = sde * 74; }
+								sde = sde + 5;
+							}
+							science = science - 1;
+						}
+						else {
+							temp = ratio + 48;
+							while sde {
+								dell = plain - pit * left +sde;
+								if engin then {
+									engin = engin -10;
+									while soc {
+										warm = cold*plain;
+										salt = warm*48;
+										slim = salt + 63;
+										fat = slim+(com-pit);
+										soc = soc - 1;
+									}
+								}
+								else {
+									engin = engin + 11;
+									if soc then {
+										warm = eat+plain;
+										salt=warm+22;
+										slim=salt-63; 
+										soc = soc + 1; }
+									else {
+										warm = cold+salt;
+										soc = soc + 22;
+									}
+									call See;
+								}
+								sde = sde - 3;
+							}
+							science = science - 1;
+						}
+						fass = fass + 23;
+					}
+					else {
+						temp = ratio * pit + status + (com - pit);
+					}
+					temp = pure +idea -eat + (com-pit);
+				}
+				eat = warm + cold * plain -salt;
+				call Ted;
+			}
+			dell = eat + (ratio*pit +status);
+			dear = 99 -dell;
+			while dear {
+				dell = 1 + temp; }
+			dell = dell + 74;
+		}
+	}
+	status = status + com - 48;
+	call Buddy;
+}
+
+procedure Buddy {
+	pit = 10;
+	pure = pit + 5;
+	temp = pure * pit + 4;
+	dell = pure + temp * 12;
+	call See;
+	dell = dell + pit + pure;
+	call Ted;
+	eat = dell + pit * pure;
+	while idea {
+		eat = pure - temp;
+		if pit then {
+			temp = temp + pit;
+			idea = temp + 1; 
+		}
+		else {
+			pure = temp + pit;
+			call Ted;
+			idea = idea - 1;
+		}
+		pure = temp + pit;
+	}
+	call Text;
+}
+
+procedure Text {
+	while idea {
+		pit = temp*3 + 2*pure;
+		call See;
+		idea = idea - 1;
+	}
+	temp = temp + pit + idea;
+}
+
+procedure Ted {
+	while dell {
+		pit = pure + pit; }
+	pure = pit + pure;
+}
+
+procedure See {
+	while eat {
+		if idea then {
+			temp = pit + 1; }
+		else {
+			call Ted;
+			pit = pit * pure + temp + eat * dell;
+		}
+	}
+}
--- RunAutotester.cmd
+++ RunAutotester.cmd
@@ -1,5 +1,6 @@
 @echo off
-Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL > result.txt
+Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL > result1.txt
 pause
-Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt result.xml >> result.txt
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt NUL > result2.txt
 pause
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test2Code.c Autotesting\CS3202-Test2Query.txt NUL > result3.txt
