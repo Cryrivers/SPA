bd21584cfabe9fc87b2ba40b5112066f6e5a26a5
Ray <u0905191@nus.edu.sg>
2013-04-17 17:08:26 +0800

    add back Next.h

---
+++ SPA/Next.h
@@ -0,0 +1,53 @@
+#ifndef NEXT_H_
+#define NEXT_H_
+
+#include "SPAType.h"
+#include "Utility.h"
+using namespace std;
+
+class Next
+{
+STMT_LIST stmt1s;
+STMT_LIST stmt2s;
+vector<CFGBipType> stmtBipType;
+
+
+BOOLEAN isNextStarHelper(STMT_LIST links, STMT stmt2, STMT_LIST* visitedNodes);
+<<<<<<< HEAD
+
+=======
+>>>>>>> 7ea746f32cf188e8aff9da525a56efbeeefe9356
+STMT_LIST getNextStarFirst(STMT stmt2);
+//STMT_LIST getNextStarFirstHelper(STMT stmt2, STMT_LIST* visitedNodes);
+void getNextStarFirstHelper(STMT stmt2, STMT_LIST* resultLst);
+STMT_LIST getNextStarSecond(STMT stmt1);
+//STMT_LIST getNextStarSecondHelper(STMT stmt1, STMT_LIST* visitedNodes);
+void getNextStarSecondHelper(STMT stmt1, STMT_LIST* resultLst);
+BOOLEAN next_00(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN next_01(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN next_10(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN next_11(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN nextStar_00(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN nextStar_01(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN nextStar_10(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+BOOLEAN nextStar_11(STMT_LIST* st1s_p, STMT_LIST* st2s_p);
+
+public:
+STMT_LIST getNextFirst(STMT stmt);
+STMT_LIST getNextSecond(STMT stmt);
+void addNext(STMT stmt1, STMT stmt2, CFGBipType bipType);
+BOOLEAN isNext(STMT stmt1, STMT stmt2);
+BOOLEAN isNextStar(STMT stmt1, STMT stmt2);
+BOOLEAN next(STMT_LIST* st1s_ptr, STMT_LIST* st2s_ptr, int arg);
+BOOLEAN nextStar(STMT_LIST* st1s_ptr, STMT_LIST* st2s_ptr, int arg);
+STMT_LIST getAllNextFirst();
+STMT_LIST getAllNextSecond();
+STMT_LIST getNextFirst(STMT stmt);
+STMT_LIST getNextSecond(STMT stmt);
+int size() {
+	return stmt1s.size();
+}
+};
+
+
+#endif /* NEXT_H_ */
