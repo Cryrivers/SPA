68d18dd6c8198ba06f48a3c9f2e40b0fcba33056
Wang Zhongliang <cryrivers@me.com>
2013-04-18 07:12:13 +0800

    Add new files for Autotester

--- RunAutotester.cmd
+++ RunAutotester.cmd
@@ -1,6 +1,6 @@
 @echo off
-Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt NUL > result1.txt
+Autotesting\SPA-Autotester.exe Autotesting\CS3201RegressionTestCode.c Autotesting\CS3201RegressionTestQuery.txt result1.xml
 pause
-Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt NUL > result2.txt
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test1Code.c Autotesting\CS3202-Test1QueryLatest.txt result2.xml
 pause
-Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test2Code.c Autotesting\CS3202-Test2Query.txt NUL > result3.txt
+Autotesting\SPA-Autotester.exe Autotesting\CS3202-Test2Code.c Autotesting\CS3202-Test2Query.txt result3.xml
---
+++ RunAutotester2.cmd
@@ -0,0 +1,2 @@
+@echo off
+Autotesting\SPA-Autotester.exe Autotesting\source1.txt Autotesting\sourcetest.txt result.xml
